5789ae7a2b1810839abcf0611e160bb96f056b1c
AwesomeMrT <tobias.sterchi@outlook.com>
2026-06-07 16:37:15 +0200

    First working version.
    New features and updates may follow soon.

---
+++ Level.java
@@ -0,0 +1,23 @@
+{
+  "schemaVersion": 1,
+  "id": "stairdoors",
+  "version": "1.0.5",
+  "name": "Stair Doors",
+  "description": "Allows doors to be placed on straight stairs when the door faces opposite the stair direction.",
+  "authors": [
+    "Tobi"
+  ],
+  "license": "MIT",
+  "environment": "*",
+  "entrypoints": {
+    "main": [
+      "com.tobi.stairdoors.StairDoorsMod"
+    ]
+  },
+  "depends": {
+    "fabricloader": ">=0.19.2",
+    "fabric-api": ">=0.150.0",
+    "minecraft": "~26.1-",
+    "java": ">=25"
+  }
+}
